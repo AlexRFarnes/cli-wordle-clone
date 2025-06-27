2b13e3ffc6dda6d9511c1825060b798f8267dff4
AlexRFarnes <alexrfarnes@gmail.com>
2025-06-28 06:59:13 +0800

    refactor the main loop

--- wyrdl.py
+++ wyrdl.py
@@ -2,22 +2,22 @@
 
 from pathlib import Path
 from random import choice
+from string import ascii_letters
 
-WORDLIST = Path("wordlist.txt")
 
-words = [
-    word.upper() for word in WORDLIST.read_text(encoding="utf-8").strip().split("\n")
-]
+def get_random_word(path_to_txt):
+    wordlist = Path(__file__).parent / path_to_txt
 
-word = choice(words)
+    words = [
+        word.upper()
+        for word in wordlist.read_text(encoding="utf-8").split("\n")
+        if len(word) == 5 and all(letter in ascii_letters for letter in word)
+    ]
 
-for guess_num in range(1, 7):
-    guess = input("Guess a word: ").upper()
+    return choice(words)
 
-    if guess == word:
-        print("Correct")
-        break
 
+def show_guess(guess, word):
     correct_letters = {
         letter for letter, correct in zip(guess, word) if letter == correct
     }
@@ -28,5 +28,28 @@ for guess_num in range(1, 7):
     print(f"Misplaced letter: {', '.join(misplaced_letters)}")
     print(f"Wrong letter: {', '.join(wrong_letters)}")
 
-else:
+
+def game_over(word):
     print(f"The word was {word}")
+
+
+def main():
+    # Pre-process
+    word = get_random_word("wordlist.txt")
+
+    # Process (main loop)
+    for guess_num in range(1, 7):
+        guess = input(f"\nGuess {guess_num}: ").upper()
+
+        show_guess(guess, word)
+        if guess == word:
+            print("Correct")
+            break
+
+    # Post-process
+    else:
+        game_over(word)
+
+
+if __name__ == "__main__":
+    main()
